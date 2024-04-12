4ce9e3476c7e4e9994ce8b44b61e7163179e77eb
Hacker32d <olemens71@gmail.com>
2024-04-12 09:29:42 -0600

    v3.0

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -118,5 +118,30 @@ function closeLightbox() {
     document.body.style.overflow = 'auto'; // Reactivar el scroll
 }
 
-  
+//swipper
+
+const carousel = document.querySelector('.carousel');
+const prevBtn = document.getElementById('prevBtn');
+const nextBtn = document.getElementById('nextBtn');
+let currentIndex = 0;
+
+nextBtn.addEventListener('click', () => {
+  if (currentIndex < carousel.children.length - 1) {
+    currentIndex++;
+    updateCarousel();
+  }
+});
+
+prevBtn.addEventListener('click', () => {
+  if (currentIndex > 0) {
+    currentIndex--;
+    updateCarousel();
+  }
+});
+
+function updateCarousel() {
+  const slideWidth = carousel.children[0].offsetWidth;
+  carousel.style.transform = `translateX(${-slideWidth * currentIndex}px)`;
+}
+
 
